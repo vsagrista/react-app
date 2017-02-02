849b3fdcf80ac78d9e7abce83390cdafabb20660
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2017-02-02 19:56:31 +0100

    Created first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+// State component
+
+class App1 extends React.Component {
+    render () {
+        return <div>
+                 <h1>This is a Component</h1>
+                 <p>Some text below it</p>
+               </div>  
+    }
 }
 
-export default App;
+// const App2 = ()=> <h1>This is a stateless component</h1>
+
+export default App1;
+
+
